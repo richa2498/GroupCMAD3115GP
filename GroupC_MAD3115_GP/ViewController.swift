3ca1e42949f89001a4783b22512680bed8df9e04
Richa Patel <richa1998.patel@gmail.com>
2019-11-18 12:19:03 -0500

    Initial Commit

---
+++ GroupC_MAD3115_GP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupC_MAD3115_GP
+//
+//  Created by MacStudent on 2019-11-18.
+//  Copyright © 2019 Make My Dish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
