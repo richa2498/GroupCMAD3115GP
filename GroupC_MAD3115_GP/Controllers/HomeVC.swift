2b362c81e0aae4dc46eee9214b712a81e6f2154c
richa <richa@1998.patel@gmail.com>
2019-11-19 13:10:11 -0500

    - Home page design added

--- GroupC_MAD3115_GP/Controllers/HomeVC.swift
+++ GroupC_MAD3115_GP/Controllers/HomeVC.swift
@@ -16,7 +16,7 @@ class HomeVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+   
     /*
     // MARK: - Navigation
 
--- GroupC_MAD3115_GP/helpers.swift
+++ GroupC_MAD3115_GP/helpers.swift
@@ -9,3 +9,13 @@
 import Foundation
 
 let BASE_URL: String = "http://www.recipepuppy.com/api/"
+//monsoon :https://www.youtube.com/results?search_query=monsoon+recipes+with+ingredients
+//summer : https://www.youtube.com/results?search_query=summer+recipes+with+ingredients
+//winter : https://www.youtube.com/results?search_query=winter+recipes+with+ingredients
+//fall   : https://www.youtube.com/results?search_query=fall+recipes+with+ingredients
+
+
+//Morning : https://www.youtube.com/results?search_query=morning+easy+breakfast
+//evening : https://www.youtube.com/results?search_query=dessert+recipes+easy+at+home
+
+
